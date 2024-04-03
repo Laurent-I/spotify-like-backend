10b53b25f6249d138bac8e341530a07f7d65eebf
Laurent-I <irakramlaw@gmail.com>
2024-04-03 23:00:28 +0200

    fix: now db connection working

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,13 +5,28 @@ import { SongsModule } from './songs/songs.module';
 import { LoggerModule } from './common/middleware/logger/logger.module';
 import { LoggerMiddleware } from './common/middleware/logger.middleware';
 import { SongsController } from './songs/songs.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
 
 @Module({
-  imports: [SongsModule, LoggerModule],
+  imports: [SongsModule, LoggerModule, TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',
+    port:5432,
+    username: 'postgres',
+    password: 'sh@d0w123',
+    database: 'test',
+    entities:[],
+    synchronize: true,
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule implements NestModule {
+  constructor( private dataSource: DataSource) {
+    console.log(dataSource.driver.database)
+  }
+
   configure(consumer: MiddlewareConsumer) {
     // consumer.apply(LoggerMiddleware).forRoutes('songs');
     // consumer.apply(LoggerMiddleware).forRoutes({path: 'songs', method: RequestMethod.POST})
